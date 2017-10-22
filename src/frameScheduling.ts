ad8bdbd763912b9b131b5c8abc2babbeeff2219d
Tom910 <tom910ru@gmail.com>
2017-10-22 23:14:55 +0300

    init commit

---
+++ src/frameScheduling.ts
@@ -0,0 +1,77 @@
+let defer: Function;
+
+if ("requestAnimationFrame" in window) {
+  defer = requestAnimationFrame.bind(window);
+} else {
+  defer = setTimeout.bind(window);
+}
+
+const timeLifeFrame = 16; // 16ms === 60fps
+
+class Job {
+  fn: Function;
+  priority: number;
+  iterations: number;
+  constructor(callback: Function, priority: number) {
+    this.fn = callback;
+    this.priority = priority;
+    this.iterations = 0;
+  }
+}
+
+export const P_LOW = 3;
+export const P_NORMAL = 5;
+export const P_HIGH = 7;
+
+const sortByPriority = (list: Job[]) => {
+  return list.sort(
+    (left: Job, right: Job) =>
+      right.priority - right.iterations - left.priority - left.iterations
+  );
+};
+
+const frameScheduling = () => {
+  const listJobs: Job[] = [];
+  let deferScheduled = false;
+
+  const runDefer = () => {
+    if (!deferScheduled) {
+      defer(runJobs);
+    }
+
+    deferScheduled = true;
+  };
+
+  const runJobs = () => {
+    const timeRun = Date.now();
+    sortByPriority(listJobs);
+
+    while (true) {
+      if (listJobs.length === 0 || Date.now() - timeRun > timeLifeFrame) {
+        break;
+      } else {
+        const job = listJobs.shift();
+
+        job && job.fn();
+      }
+    }
+
+    deferScheduled = false;
+
+    if (listJobs.length !== 0) {
+      for (var i = 0; i < listJobs.length; i++) {
+        listJobs[i].iterations++;
+      }
+
+      runDefer();
+    }
+  };
+
+  return (callback: Function, { priority = P_NORMAL } = {}) => {
+    listJobs.push(new Job(callback, priority));
+
+    runDefer();
+  };
+};
+
+export default frameScheduling();
---
+++ tests/frameScheduling.ts
@@ -0,0 +1,139 @@
+import frameScheduling, { P_LOW, P_HIGH } from "../src/frameScheduling";
+
+const mockDataNow = () => {
+  let result = 1000;
+  let count = 0;
+
+  Date.now = () => {
+    if (count === 3) {
+      result = 1000;
+      count = 0;
+    }
+
+    if (count === 0) {
+      count++;
+      return result;
+    }
+
+    count++;
+    return (result += 10);
+  };
+};
+
+describe("frameScheduling", () => {
+  const originDateNow = Date.now.bind(Date);
+
+  beforeEach(() => {
+    setTimeout.mockClear();
+  });
+
+  afterEach(() => {
+    Date.now = originDateNow;
+  });
+
+  it("Run multi tasks in 1 frame", () => {
+    let counter = 0;
+
+    frameScheduling(() => {
+      counter++;
+    });
+    frameScheduling(() => {
+      counter++;
+    });
+    frameScheduling(() => {
+      counter++;
+    });
+    jest.runOnlyPendingTimers();
+
+    expect(setTimeout.mock.calls.length).toBe(1);
+    expect(counter).toBe(3);
+  });
+
+  it("Run multi tasks in multi frames", () => {
+    let counter = 0;
+    mockDataNow();
+
+    frameScheduling(() => {
+      counter++;
+    });
+    frameScheduling(() => {
+      counter++;
+    });
+    frameScheduling(() => {
+      counter++;
+    });
+    frameScheduling(() => {
+      counter++;
+    });
+
+    jest.runAllTimers();
+
+    expect(setTimeout.mock.calls.length).toBe(4);
+    expect(counter).toBe(4);
+  });
+
+  it("Simple priority", () => {
+    let result = [];
+
+    frameScheduling(
+      () => {
+        result.push("Vue");
+      },
+      { priority: P_LOW }
+    );
+    frameScheduling(() => {
+      result.push("Angular");
+    });
+    frameScheduling(
+      () => {
+        result.push("Ember");
+      },
+      { priority: P_LOW }
+    );
+    frameScheduling(
+      () => {
+        result.push("React");
+      },
+      { priority: P_HIGH }
+    );
+
+    jest.runOnlyPendingTimers();
+
+    expect(setTimeout.mock.calls.length).toBe(1);
+    expect(result).toEqual(["React", "Angular", "Vue", "Ember"]);
+  });
+
+  it("Priority with upfiling iterations", () => {
+    let result = [];
+    mockDataNow();
+
+    frameScheduling(() => result.push("Bye"), { priority: 0 });
+    frameScheduling(() => result.push("A"), { priority: 1 });
+    jest.runOnlyPendingTimers();
+    frameScheduling(() => result.push("Al"), { priority: 2 });
+    jest.runOnlyPendingTimers();
+
+    frameScheduling(() => result.push("Alo"), { priority: 2 });
+    jest.runOnlyPendingTimers();
+
+    jest.runOnlyPendingTimers();
+    expect(result).toEqual(["A", "Al", "Bye", "Alo"]);
+  });
+
+  it("Priority with many runs", () => {
+    let result = 0;
+    mockDataNow();
+
+    frameScheduling(() => (result *= 2), { priority: 0 });
+    frameScheduling(() => (result *= 3), { priority: 49 });
+
+    for (var i = 0; i < 100; i++) {
+      frameScheduling(() => (result += 1), { priority: 90 });
+      jest.runOnlyPendingTimers();
+    }
+
+    jest.runOnlyPendingTimers();
+    jest.runOnlyPendingTimers();
+    expect(result).toEqual(354);
+  });
+});
